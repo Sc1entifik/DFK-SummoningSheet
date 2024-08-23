3fe3e48158b469f99d916c10324dc0cef3010092
Scientifik <scientifik1@protonmail.com>
2024-08-23 12:05:48 -0400

    Created mats_form.html for mats mining profitability tool. Gathered and gimped lots of photos for website. Minor improvements to code base

--- app.py
+++ app.py
@@ -1,38 +1,49 @@
 from flask import Flask, render_template, redirect, url_for
 
-from forms import HeroInputForm
+from forms import HeroInputForm, MatsInputForm
 from controller import StatsController
 
 app = Flask(__name__)
-app.secret_key = 'ieropajdklsagnfjklghrkjlffjdklasfjkdsa;'
+app.secret_key = "ieropajdklsagnfjklghrkjlffjdklasfjkdsa;"
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def index():
     form = HeroInputForm()
 
     if form.validate_on_submit():
         controller = StatsController(form)
 
-        return render_template('summon_sheet.html', controller=controller.optimized_summon_odds_list())
+        return render_template("summon_sheet.html", controller=controller.optimized_summon_odds_list())
 
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/hero_form/', methods=['GET', 'POST'])
+@app.route("/hero_form/", methods=["GET", "POST"])
 def hero_form():
     form = HeroInputForm()
-    return render_template('hero_form.html', form=form)
+    return render_template("hero_form.html", form=form)
 
 
-@app.route('/about/')
+@app.route("/mats_form/", methods=["GET", "POST"])
+def mats_form():
+    form = MatsInputForm()
+
+    if form.validate_on_submit():
+        bloater = form.transaction_cost
+        print(bloater)
+
+    return render_template("mats_form.html", form=form)
+
+
+@app.route("/about/")
 def about():
-    return render_template('about.html')
+    return render_template("about.html")
 
 
 @app.errorhandler(Exception)
 def failure_form(e):
-    return render_template('failure.html')
+    return render_template("failure.html")
 
 
 if __name__ == "__main__":
